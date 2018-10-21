022d04171a6de640faa1329edb5d7bc8860e6b83
Ali Sultan <ajasultan@gmail.com>
2018-10-21 23:04:46 +0400

    store inputs from user into variables

--- designs.js
+++ designs.js
@@ -2,9 +2,15 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const canvasTable = document.querySelector('#pixelCanvas'); //canvasTable
+var pickedHeight = document.querySelector('#inputHeight').value; //Input Height
+var pickedWidth = document.querySelector('#inputWidth').value; //Input Width
+var pickedColor =  document.querySelector('#colorPicker').value; //Choosen Color
+
 
 function makeGrid() {
     var customDiv = document.createElement('div');
+    customDiv.style.cssText = 'border-color: black; width: 1px; height: 1px;';
 // Your code goes here!
 
 }
