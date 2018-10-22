47b460ebf8adbdbd20ab8b37e3e1fbfdceef035c
Ali Sultan <ajasultan@gmail.com>
2018-10-22 19:42:29 +0400

    write makeGrid function to create the canvas

--- designs.js
+++ designs.js
@@ -3,17 +3,24 @@ const canvasTable = document.querySelector('#pixelCanvas');
 const boxStyle =  'border: solid black; width:1em; height: 1em;';
 
 
-function createPxlBox(){
-    var customDiv = document.createElement('div');
-    customDiv.style.cssText = 'border: solid black; width: 1px; height: 1px;';
-}
-
-
-function makeGrid() {
-// check the Width
-// Check the Height
-// Create boxes of the size width x Height
-
-// Your code goes here!
-
+/* makeGrid is a function that takes the input from the user input and creates
+ a canvas of size inputWidth x inputHeight */
+function makeGrid(){
+    //Store Input Height
+    var pickedHeight = document.querySelector('#inputHeight').value;
+    //Store Input Width
+    var pickedWidth = document.querySelector('#inputWidth').value;
+    //Create a Canvas of Column = width & Row = Height
+    for(var column = 0; column < pickedHeight; column++){
+        // Create Column in HTML Table by Tag 'tr'
+        var customColumn = document.createElement('tr');
+        canvasTable.appendChild(customColumn);
+        for (var row = 0; row < pickedWidth; row++){
+            // Create row in HTML Table by Tag 'th'
+            var customRow = document.createElement('th');
+            customColumn.appendChild(customRow);
+            customColumn.style.cssText = boxStyle;
+            customRow.style.cssText = boxStyle;
+        }
+    }
 }
