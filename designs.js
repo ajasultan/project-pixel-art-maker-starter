c195c3b7d23ce731231b72e5a2c8a181d05ad5f7
Ali Sultan <ajasultan@gmail.com>
2018-10-22 08:06:00 +0400

    add create Pixel Box

--- designs.js
+++ designs.js
@@ -8,9 +8,17 @@ var pickedWidth = document.querySelector('#inputWidth').value; //Input Width
 var pickedColor =  document.querySelector('#colorPicker').value; //Choosen Color
 
 
-function makeGrid() {
+function createPxlBox(){
     var customDiv = document.createElement('div');
-    customDiv.style.cssText = 'border-color: black; width: 1px; height: 1px;';
+    customDiv.style.cssText = 'border: solid black; width: 1px; height: 1px;';
+}
+
+
+function makeGrid() {
+// check the Width
+// Check the Height
+// Create boxes of the size width x Height
+
 // Your code goes here!
 
 }
